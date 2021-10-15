fc819f717d0a28e8921920f24b0b4892950f803e
ikorennoy <korennoy.ilya@gmail.com>
2021-10-15 19:47:49 +0300

    add open tests

--- src/test/java/one/jasyncfio/BufferedFileTest.java
+++ src/test/java/one/jasyncfio/BufferedFileTest.java
@@ -3,15 +3,45 @@ package one.jasyncfio;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BufferedFileTest {
 
     @Test
-    void open() throws ExecutionException, InterruptedException {
-        CompletableFuture<BufferedFile> open = BufferedFile.open("./tmp/file1");
+    void open() throws ExecutionException, InterruptedException, IOException {
+        File file = File.createTempFile("temp-", "-file");
+        file.deleteOnExit();
+        CompletableFuture<BufferedFile> open = BufferedFile.open(file.getPath());
+        waitCompletion(open);
+        assertTrue(open.isDone());
         BufferedFile bufferedFile = open.get();
-        System.out.println(bufferedFile.getFd());
+        assertTrue(bufferedFile.getFd() > 0);
+    }
+
+    @Test
+    void open_fileNotExist() throws InterruptedException {
+        CompletableFuture<BufferedFile> open = BufferedFile.open("some/path");
+        waitCompletion(open);
+        assertTrue(open.isDone());
+        assertTrue(open.isCompletedExceptionally());
+        assertThrows(ExecutionException.class, open::get);
+    }
+
+
+
+    private void waitCompletion(CompletableFuture<?> future) throws InterruptedException {
+        int cnt = 0;
+        while (!future.isDone()) {
+            if (cnt > 5) {
+                break;
+            }
+            Thread.sleep(100);
+            cnt++;
+        }
     }
 }
