93a4328be06ee1dd32bc594bd3f89786e30cc035
ikorennoy <korennoy.ilya1@gmail.com>
2021-11-06 13:33:40 +0300

    try build in docker

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -7,7 +7,7 @@ deb http://deb.debian.org/debian stretch main
 deb http://security.debian.org/debian-security stretch/updates main
 END
 
-cd "$(dirname "$(dirname "$0")")"
+#cd "$(dirname "$(dirname "$0")")"
 
 apt-get update -y
 apt-get install -y --no-install-recommends openjdk-8-jdk-headless make gcc libc6-dev
