82a2784be6f7e348741a9ef3489cf644f35d0e34
ikorennoy <korennoy.ilya1@gmail.com>
2021-11-06 13:25:42 +0300

    try build in docker

---
+++ .github/workflows/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+cd "$(dirname "$(dirname "$0")")"
+
+apt-get update -y
+apt-get install -y --no-install-recommends openjdk-8-jdk-headless make gcc libc6-dev
+./gradlew build
+
