7923d606cf7f51845b81245bafb90558f5788275
ikorennoy <korennoy.ilya1@gmail.com>
2021-11-06 17:49:42 +0300

    1. CI 2. JNI exception and util methods

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,15 +2,8 @@
 
 set -ex
 
-cat <<END > /etc/apt/sources.list.d/stretch.list
-deb http://deb.debian.org/debian stretch main
-deb http://security.debian.org/debian-security stretch/updates main
-END
-
-cd "$(dirname "$(dirname "$0")")"
+cd "$(dirname "$(dirname "$0")")"/..
 
 apt-get update -y
-apt-get install -y --no-install-recommends openjdk-8-jdk-headless build-essential
-cd ..
-./gradlew build
-
+apt-get install -y --no-install-recommends openjdk-8-jdk make gcc g++ libc6-dev
+./gradlew build --debug
