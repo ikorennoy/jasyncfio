3125f907c3a168b2fe8dc16c0acf1116286369bf
ikorennoy <korennoy.ilya1@gmail.com>
2022-01-02 15:44:07 +0300

    delete rename

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,31 +12,9 @@ repositories {
     mavenCentral()
 }
 
-tasks.build {
-    dependsOn.add(tasks.getByPath(":renameNativeLib"))
-}
-
-tasks.withType(CCompile::class.java) {
-    compilerArgs.add("-O3")
+//tasks.withType(CCompile::class.java) {
 //    compilerArgs.add("-ggdb")
-}
-
-
-tasks.register("renameNativeLib", Copy::class.java) {
-    dependsOn(tasks.getByPath(":sharedLibrary"))
-    var arch = System.getProperty("os.arch", "unknown")
-    arch = if (arch.equals("x86_64", true) || arch.equals("amd64", true)) {
-        "x86_64"
-    } else if (arch.equals("aarch64", true)) {
-        "aarch64"
-    } else {
-        throw StopActionException("$arch is not supported")
-    }
-    from("build/libs/main")
-    include("libjasyncfio.so")
-    destinationDir = file("build/target")
-    rename("libjasyncfio.so", "libjasyncfio-$arch.so")
-}
+//}
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
