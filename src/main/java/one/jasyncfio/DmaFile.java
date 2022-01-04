9a58f648550e3a620b2e34e84acb393ba77edf54
ikorennoy <korennoy.ilya1@gmail.com>
2022-01-04 17:13:36 +0300

    Support ByteBuffer aligned allocate

--- src/main/java/one/jasyncfio/DmaFile.java
+++ src/main/java/one/jasyncfio/DmaFile.java
@@ -3,9 +3,14 @@ package one.jasyncfio;
 import one.jasyncfio.natives.MemoryUtils;
 import one.jasyncfio.natives.Native;
 
+import java.nio.ByteBuffer;
 import java.util.concurrent.CompletableFuture;
 
+import static one.jasyncfio.natives.MemoryUtils.allocateAlignedByteBuffer;
+
 public class DmaFile extends AbstractFile {
+    private final int readAlignment = 512;
+    private final int writeAlignment = 4096;
 
     DmaFile(int fd, String path, long pathAddress) {
         super(fd, path, pathAddress);
@@ -22,8 +27,9 @@ public class DmaFile extends AbstractFile {
             throw new IllegalArgumentException("path must be not null");
         }
         long pathPtr = MemoryUtils.getStringPtr(path);
+        int flags = Native.O_RDONLY | Native.O_CLOEXEC | Native.O_DIRECT;
         CompletableFuture<Integer> futureFd =
-                EventExecutorGroup.get().scheduleOpenAt(-1, pathPtr, Native.O_RDONLY | Native.O_DIRECT, 0);
+                EventExecutorGroup.get().scheduleOpenAt(-1, pathPtr, flags, 0);
         return futureFd
                 .thenApply((fd) -> new DmaFile(fd, path, pathPtr));
     }
@@ -46,4 +52,37 @@ public class DmaFile extends AbstractFile {
         return futureFd
                 .thenApply((fd) -> new BufferedFile(fd, path, pathPtr));
     }
+
+    /**
+     * Reads data from a specified position and of a specified length into a byte buffer.
+     * <p>
+     * It is not necessary to respect the `O_DIRECT` alignment of the file, and
+     * this API will internally convert the positions and sizes to match, at a cost.
+     * <p>
+     * limit of the result ByteBuffer will be installed at read bytes
+     *
+     * @param position The file position at which the transfer is to begin; must be non-negative
+     * @param length   The content length; must be non-negative
+     * @return {@link CompletableFuture} with the result ByteBuffer
+     */
+    public CompletableFuture<ByteBuffer> readAligned(long position, int length) {
+        final long effectivePosition = alignDown(position, readAlignment);
+        final long b = (position - effectivePosition);
+        final int effectiveSize = alignUp((length + b), readAlignment);
+        ByteBuffer byteBuffer = allocateAlignedByteBuffer(effectiveSize, readAlignment);
+        CompletableFuture<Integer> read = read(effectivePosition, effectiveSize, byteBuffer);
+        return read.thenApply((result) -> {
+            byteBuffer.limit(Math.min(result, length));
+            return byteBuffer;
+        });
+    }
+
+    public static int alignUp(long v, long align) {
+        return (int) ((v + align - 1) & -align);
+    }
+
+    public static int alignDown(long v, long align) {
+        return (int) (v & -align);
+    }
+
 }
