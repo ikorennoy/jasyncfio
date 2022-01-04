29bb93b294b638d547a380f6c1d93de1c85a8d80
Ilya Korennoy <korennoy.ilya@gmail.com>
2022-01-04 06:59:11 +0300

    add AbstractFile

---
+++ src/main/java/one/jasyncfio/AbstractFile.java
@@ -0,0 +1,160 @@
+package one.jasyncfio;
+
+import one.jasyncfio.natives.MemoryUtils;
+import one.jasyncfio.natives.Native;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.CompletableFuture;
+
+class AbstractFile {
+    final int fd;
+    final String path;
+    final long pathAddress;
+
+
+    AbstractFile(int fd, String path, long pathAddress) {
+        this.fd = fd;
+        this.path = path;
+        this.pathAddress = pathAddress;
+    }
+
+
+    public int getRawFd() {
+        return fd;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * Reads data from a specified position and of a specified length into a byte buffer.
+     *
+     * @param position The file position at which the transfer is to begin; must be non-negative
+     * @param length   The content length; must be non-negative
+     * @param buffer   The buffer from which bytes are to be retrieved
+     * @return {@link CompletableFuture} with the number of bytes read
+     */
+    public CompletableFuture<Integer> read(long position, int length, ByteBuffer buffer) {
+        if (buffer == null) {
+            throw new IllegalArgumentException("buffer must be not null");
+        } else if (position < 0L) {
+            throw new IllegalArgumentException("position must be positive");
+        } else if (length < 0L) {
+            throw new IllegalArgumentException("length must be positive");
+        }
+        return EventExecutorGroup.get()
+                .scheduleRead(fd, MemoryUtils.getDirectBufferAddress(buffer), position, length);
+    }
+
+    /**
+     * Reads data at the specified position into a buffer.
+     *
+     * @param position start position
+     * @param buffer   The buffer into which bytes are to be transferred
+     * @return {@link CompletableFuture} with the number of bytes read
+     */
+    public CompletableFuture<Integer> read(long position, ByteBuffer buffer) {
+        return read(position, buffer.limit(), buffer);
+    }
+
+    /**
+     * Write the data with in the byte buffer the specified length starting at the given file position.
+     * If the given position is greater than the file's current size then the file will be grown to accommodate the new bytes;
+     * the values of any bytes between the previous end-of-file and the newly-written bytes are unspecified.
+     *
+     * @param position The file position at which the transfer is to begin; must be non-negative
+     * @param length   The content length; must be non-negative
+     * @param buffer   The buffer from which bytes are to be retrieved
+     * @return {@link CompletableFuture} with the number of bytes written
+     */
+
+    public CompletableFuture<Integer> write(long position, int length, ByteBuffer buffer) {
+        if (buffer == null) {
+            throw new IllegalArgumentException("buffer must be not null");
+        } else if (position < 0L) {
+            throw new IllegalArgumentException("position must be positive");
+        } else if (length < 0L) {
+            throw new IllegalArgumentException("length must be positive");
+        }
+        return EventExecutorGroup.get()
+                .scheduleWrite(fd, MemoryUtils.getDirectBufferAddress(buffer), position, length);
+    }
+
+    /**
+     * Returns the size of a file, in bytes.
+     *
+     * @return {@link CompletableFuture} with file size
+     */
+    public CompletableFuture<Long> size() {
+        long bufAddress = MemoryUtils.allocateMemory(StatxUtils.BUF_SIZE);
+        return EventExecutorGroup.get()
+                .scheduleStatx(-1, pathAddress, 0, Native.STATX_SIZE, bufAddress)
+                .thenApply((r) -> {
+                    long size = StatxUtils.getSize(bufAddress);
+                    MemoryUtils.freeMemory(bufAddress);
+                    return size;
+                });
+    }
+
+    /**
+     * Issues fdatasync for the underlying file, instructing the OS to flush all writes to the device,
+     * providing durability even if the system crashes or is rebooted.
+     */
+    public CompletableFuture<Integer> dataSync() {
+        return EventExecutorGroup.get().scheduleFsync(fd, Native.IORING_FSYNC_DATASYNC);
+    }
+
+    /**
+     * Pre-allocates space in the filesystem to hold a file at least as big as the size argument from 0 offset.
+     *
+     * @param size bytes to allocate; must be non-negative
+     */
+    public CompletableFuture<Integer> preAllocate(long size) {
+        return preAllocate(size, 0);
+    }
+
+    /**
+     * Pre-allocates space in the filesystem to hold a file at least as big as the size argument from specified offset.
+     * After a successful call, subsequent writes into the range
+     * specified by offset and len are guaranteed not to fail because of
+     * lack of disk space.
+     *
+     * @param size   bytes to allocate; must be non-negative
+     * @param offset start offset; must be non-negative
+     */
+    public CompletableFuture<Integer> preAllocate(long size, long offset) {
+        if (size < 0) {
+            throw new IllegalArgumentException("size must be positive");
+        }
+        if (offset < 0) {
+            throw new IllegalArgumentException("offset must be positive");
+        }
+        return EventExecutorGroup.get().scheduleFallocate(fd, size, 0, offset);
+    }
+
+
+    /**
+     * Remove this file.
+     * <p>
+     * The file does not have to be closed to be removed.
+     * Removing removes the name from the filesystem but the file will still be accessible for as long as it is open.
+     */
+    public CompletableFuture<Integer> remove() {
+        return EventExecutorGroup.get()
+                .scheduleUnlink(-1, pathAddress, 0);
+    }
+
+    @Override
+    protected void finalize() {
+        MemoryUtils.releaseString(path, pathAddress);
+    }
+
+    /**
+     * Closes this file.
+     */
+    public CompletableFuture<Integer> close() {
+        return EventExecutorGroup.get().scheduleClose(fd);
+    }
+
+}
--- src/main/java/one/jasyncfio/BufferedFile.java
+++ src/main/java/one/jasyncfio/BufferedFile.java
@@ -3,27 +3,12 @@ package one.jasyncfio;
 import one.jasyncfio.natives.MemoryUtils;
 import one.jasyncfio.natives.Native;
 
-import java.nio.ByteBuffer;
 import java.util.concurrent.CompletableFuture;
 
-public class BufferedFile {
-    private final int fd;
-    private final String path;
-    private final long pathAddress;
+public class BufferedFile extends AbstractFile {
 
-    // todo sort args
-    private BufferedFile(String path, int fd, long pathAddress) {
-        this.path = path;
-        this.fd = fd;
-        this.pathAddress = pathAddress;
-    }
-
-    public int getRawFd() {
-        return fd;
-    }
-
-    public String getPath() {
-        return path;
+    BufferedFile(int fd, String path, long pathAddress) {
+        super(fd, path, pathAddress);
     }
 
     /**
@@ -40,7 +25,7 @@ public class BufferedFile {
         CompletableFuture<Integer> futureFd =
                 EventExecutorGroup.get().scheduleOpenAt(-1, pathPtr, Native.O_RDONLY, 0);
         return futureFd
-                .thenApply((fd) -> new BufferedFile(path, fd, pathPtr));
+                .thenApply((fd) -> new BufferedFile(fd, path, pathPtr));
     }
 
     /**
@@ -59,136 +44,6 @@ public class BufferedFile {
         CompletableFuture<Integer> futureFd =
                 EventExecutorGroup.get().scheduleOpenAt(-1, pathPtr, flags, 0666);
         return futureFd
-                .thenApply((fd) -> new BufferedFile(path, fd, pathPtr));
-    }
-
-    /**
-     * Reads data from a specified position and of a specified length into a byte buffer.
-     *
-     * @param position The file position at which the transfer is to begin; must be non-negative
-     * @param length   The content length; must be non-negative
-     * @param buffer   The buffer from which bytes are to be retrieved
-     * @return {@link CompletableFuture} with the number of bytes read
-     */
-    public CompletableFuture<Integer> read(long position, int length, ByteBuffer buffer) {
-        if (buffer == null) {
-            throw new IllegalArgumentException("buffer must be not null");
-        } else if (position < 0L) {
-            throw new IllegalArgumentException("position must be positive");
-        } else if (length < 0L) {
-            throw new IllegalArgumentException("length must be positive");
-        }
-        return EventExecutorGroup.get()
-                .scheduleRead(fd, MemoryUtils.getDirectBufferAddress(buffer), position, length);
-    }
-
-    /**
-     * Reads data at the specified position into a buffer.
-     *
-     * @param position start position
-     * @param buffer   The buffer into which bytes are to be transferred
-     * @return {@link CompletableFuture} with the number of bytes read
-     */
-    public CompletableFuture<Integer> read(long position, ByteBuffer buffer) {
-        return read(position, buffer.limit(), buffer);
-    }
-
-    /**
-     * Write the data with in the byte buffer the specified length starting at the given file position.
-     * If the given position is greater than the file's current size then the file will be grown to accommodate the new bytes;
-     * the values of any bytes between the previous end-of-file and the newly-written bytes are unspecified.
-     *
-     * @param position The file position at which the transfer is to begin; must be non-negative
-     * @param length   The content length; must be non-negative
-     * @param buffer   The buffer from which bytes are to be retrieved
-     * @return {@link CompletableFuture} with the number of bytes written
-     */
-
-    public CompletableFuture<Integer> write(long position, int length, ByteBuffer buffer) {
-        if (buffer == null) {
-            throw new IllegalArgumentException("buffer must be not null");
-        } else if (position < 0L) {
-            throw new IllegalArgumentException("position must be positive");
-        } else if (length < 0L) {
-            throw new IllegalArgumentException("length must be positive");
-        }
-        return EventExecutorGroup.get()
-                .scheduleWrite(fd, MemoryUtils.getDirectBufferAddress(buffer), position, length);
-    }
-
-    /**
-     * Returns the size of a file, in bytes.
-     *
-     * @return {@link CompletableFuture} with file size
-     */
-    public CompletableFuture<Long> size() {
-        long bufAddress = MemoryUtils.allocateMemory(StatxUtils.BUF_SIZE);
-        return EventExecutorGroup.get()
-                .scheduleStatx(-1, pathAddress, 0, Native.STATX_SIZE, bufAddress)
-                .thenApply((r) -> {
-                    long size = StatxUtils.getSize(bufAddress);
-                    MemoryUtils.freeMemory(bufAddress);
-                    return size;
-                });
-    }
-
-    /**
-     * Issues fdatasync for the underlying file, instructing the OS to flush all writes to the device,
-     * providing durability even if the system crashes or is rebooted.
-     */
-    public CompletableFuture<Integer> dataSync() {
-        return EventExecutorGroup.get().scheduleFsync(fd, Native.IORING_FSYNC_DATASYNC);
-    }
-
-    /**
-     * Pre-allocates space in the filesystem to hold a file at least as big as the size argument from 0 offset.
-     *
-     * @param size bytes to allocate; must be non-negative
-     */
-    public CompletableFuture<Integer> preAllocate(long size) {
-        return preAllocate(size, 0);
-    }
-
-    /**
-     * Pre-allocates space in the filesystem to hold a file at least as big as the size argument from specified offset.
-     * After a successful call, subsequent writes into the range
-     * specified by offset and len are guaranteed not to fail because of
-     * lack of disk space.
-     *
-     * @param size   bytes to allocate; must be non-negative
-     * @param offset start offset; must be non-negative
-     */
-    public CompletableFuture<Integer> preAllocate(long size, long offset) {
-        if (size < 0) {
-            throw new IllegalArgumentException("size must be positive");
-        }
-        if (offset < 0) {
-            throw new IllegalArgumentException("offset must be positive");
-        }
-        return EventExecutorGroup.get().scheduleFallocate(fd, size, 0, offset);
-    }
-
-
-    /**
-     * Remove this file.
-     * <p>
-     * The file does not have to be closed to be removed.
-     * Removing removes the name from the filesystem but the file will still be accessible for as long as it is open.
-     */
-    public CompletableFuture<Integer> remove() {
-        return EventExecutorGroup.get()
-                .scheduleUnlink(-1, pathAddress, 0);
-    }
-
-    @Override
-    protected void finalize() {
-        MemoryUtils.releaseString(path, pathAddress);
-    }
-
-    /**
-     * Closes this file.
-     */
-    public CompletableFuture<Integer> close() {
-        return EventExecutorGroup.get().scheduleClose(fd);
+                .thenApply((fd) -> new BufferedFile(fd, path, pathPtr));
     }
 }
