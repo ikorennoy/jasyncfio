993a1f5f1e117cc26067a65dfc73bc3f0f041bad
ikorennoy <korennoy.ilya@gmail.com>
2022-01-05 18:33:36 +0300

    improve tests

--- src/test/java/one/jasyncfio/BufferedFileTest.java
+++ src/test/java/one/jasyncfio/BufferedFileTest.java
@@ -1,10 +1,13 @@
 package one.jasyncfio;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -17,10 +20,9 @@ public class BufferedFileTest {
     private final String TEMP_FILE_NAME = "/tmp/" + UUID.randomUUID();
 
     @Test
-    void open() throws Exception {
-        File file = File.createTempFile("temp-", "-file");
-        file.deleteOnExit();
-        BufferedFile bufferedFile = BufferedFile.open(file.getPath()).get(1000, TimeUnit.MILLISECONDS);
+    void open(@TempDir Path tmpDir) throws Exception {
+        Path file = Files.createTempFile(tmpDir, "temp-", "-file");
+        BufferedFile bufferedFile = BufferedFile.open(file.toString()).get(1000, TimeUnit.MILLISECONDS);
         assertTrue(bufferedFile.getRawFd() > 0);
     }
 
@@ -37,20 +39,15 @@ public class BufferedFileTest {
     }
 
     @Test
-    void create_fileExist() throws Exception {
-        File file = File.createTempFile("temp-", "-file");
-        file.deleteOnExit();
-        assertTrue(file.exists());
-        FileWriter writer = new FileWriter(file);
-        writer.write("file is not empty");
-        writer.flush();
-        writer.close();
-        assertTrue(file.length() > 0);
-        BufferedFile bufferedFile = BufferedFile.create(file.getPath()).get(1000, TimeUnit.MILLISECONDS);
+    void create_fileExist(@TempDir Path tmpDir) throws Exception {
+        Path file = Files.createTempFile(tmpDir, "temp-", "-file");
+        writeStringToFile("file is not empty", file.toFile());
+        assertTrue(Files.size(file) > 0);
+        BufferedFile bufferedFile = BufferedFile.create(file.toString()).get(1000, TimeUnit.MILLISECONDS);
         // file was actually open
         assertTrue(bufferedFile.getRawFd() > 0);
         // file was truncated
-        assertEquals(0, file.length());
+        assertEquals(0, Files.size(file));
     }
 
     @Test
@@ -63,93 +60,73 @@ public class BufferedFileTest {
     }
 
     @Test
-    void read() throws Exception {
-        File tempFile = File.createTempFile("temp-", "-file");
-        tempFile.deleteOnExit();
-        FileWriter fw = new FileWriter(tempFile);
+    void read(@TempDir Path tmpDir) throws Exception {
+        Path tempFile = Files.createTempFile(tmpDir, "temp-", "-file");
         String resultString = prepareString(100);
-        fw.write(resultString);
-        fw.flush();
-        fw.close();
-        assertTrue(tempFile.length() > 0);
-        ByteBuffer bb = ByteBuffer.allocateDirect((int) tempFile.length());
-        BufferedFile bufferedFile = BufferedFile.open(tempFile.getPath()).get(1000, TimeUnit.MILLISECONDS);
+        writeStringToFile(resultString, tempFile.toFile());
+        assertTrue(Files.size(tempFile) > 0);
+        ByteBuffer bb = ByteBuffer.allocateDirect((int) Files.size(tempFile));
+        BufferedFile bufferedFile = BufferedFile.open(tempFile.toString()).get(1000, TimeUnit.MILLISECONDS);
         Integer bytes = bufferedFile.read(0, bb).get(1000, TimeUnit.MILLISECONDS);
-        assertEquals((int) tempFile.length(), bytes);
+        assertEquals((int) Files.size(tempFile), bytes);
         assertEquals(resultString, StandardCharsets.UTF_8.decode(bb).toString());
     }
 
     @Test
-    void read_wrongBuffer() throws Exception {
-        File tempFile = File.createTempFile("temp-", "-file");
-        tempFile.deleteOnExit();
-        BufferedFile bufferedFile = BufferedFile.open(tempFile.getPath()).get(1000, TimeUnit.MILLISECONDS);
+    void read_wrongBuffer(@TempDir Path tmpDir) throws Exception {
+        Path tempFile = Files.createTempFile(tmpDir, "temp-", "-file");
+        BufferedFile bufferedFile = BufferedFile.open(tempFile.toString()).get(1000, TimeUnit.MILLISECONDS);
         assertThrows(IllegalArgumentException.class, () -> bufferedFile.read(0, ByteBuffer.allocate(10)));
     }
 
     @Test
-    void read_bufferGreaterThanFile() throws Exception {
-        File tempFile = File.createTempFile("temp-", "-file");
-        tempFile.deleteOnExit();
-        FileWriter fw = new FileWriter(tempFile);
+    void read_bufferGreaterThanFile(@TempDir Path tmpDir) throws Exception {
+        Path tempFile = Files.createTempFile(tmpDir, "temp-", "-file");
         String resultString = prepareString(100);
-        fw.write(resultString);
-        fw.flush();
-        fw.close();
+        writeStringToFile(resultString, tempFile.toFile());
         int stringLength = resultString.getBytes(StandardCharsets.UTF_8).length;
         ByteBuffer byteBuffer = ByteBuffer.allocateDirect(stringLength * 2);
-        BufferedFile bufferedFile = BufferedFile.open(tempFile.getPath()).get(1000, TimeUnit.MILLISECONDS);
+        BufferedFile bufferedFile = BufferedFile.open(tempFile.toString()).get(1000, TimeUnit.MILLISECONDS);
         Integer bytes = bufferedFile.read(0, byteBuffer).get(1000, TimeUnit.MILLISECONDS);
         assertEquals(stringLength, bytes);
     }
 
     @Test
-    void read_bufferLessThanFile() throws Exception {
-        File tempFile = File.createTempFile("temp-", "-file");
-        tempFile.deleteOnExit();
-        FileWriter fw = new FileWriter(tempFile);
+    void read_bufferLessThanFile(@TempDir Path tmpDir) throws Exception {
+        Path tempFile = Files.createTempFile(tmpDir, "temp-", "-file");
         String resultString = prepareString(100);
-        fw.write(resultString);
-        fw.flush();
-        fw.close();
+        writeStringToFile(resultString, tempFile.toFile());
         int stringLength = resultString.getBytes(StandardCharsets.UTF_8).length;
         ByteBuffer byteBuffer = ByteBuffer.allocateDirect(stringLength / 2);
-        BufferedFile bufferedFile = BufferedFile.open(tempFile.getPath()).get(1000, TimeUnit.MILLISECONDS);
+        BufferedFile bufferedFile = BufferedFile.open(tempFile.toString()).get(1000, TimeUnit.MILLISECONDS);
         Integer bytes = bufferedFile.read(0, byteBuffer).get(1000, TimeUnit.MILLISECONDS);
         assertEquals(stringLength / 2, bytes);
     }
 
     @Test
-    void read_offsetGreaterThanFileSize() throws Exception {
-        File tempFile = File.createTempFile("temp-", "-file");
-        tempFile.deleteOnExit();
-        FileWriter fw = new FileWriter(tempFile);
+    void read_offsetGreaterThanFileSize(@TempDir Path tmpDir) throws Exception {
+        Path tempFile = Files.createTempFile(tmpDir, "temp-", "-file");
         String resultString = prepareString(100);
-        fw.write(resultString);
-        fw.flush();
-        fw.close();
+        writeStringToFile(resultString, tempFile.toFile());
         int stringLength = resultString.getBytes(StandardCharsets.UTF_8).length;
         ByteBuffer byteBuffer = ByteBuffer.allocateDirect(stringLength);
-        BufferedFile bufferedFile = BufferedFile.open(tempFile.getPath()).get(1000, TimeUnit.MILLISECONDS);
+        BufferedFile bufferedFile = BufferedFile.open(tempFile.toString()).get(1000, TimeUnit.MILLISECONDS);
         Integer bytes = bufferedFile.read(stringLength * 2L, byteBuffer).get(1000, TimeUnit.MILLISECONDS);
         assertEquals(0, bytes);
     }
 
-    // read len 0
-
     @Test
-    void write_emptyFile() throws Exception {
-        File tempFile = File.createTempFile("temp-", "-file");
-        tempFile.deleteOnExit();
-        BufferedFile bufferedFile = BufferedFile.create(tempFile.getPath()).get(1000, TimeUnit.MILLISECONDS);
-        assertEquals(0, tempFile.length());
+    void write_emptyFile(@TempDir Path tmpDir) throws Exception {
+        Path tempFile = Files.createTempFile(tmpDir, "temp-", "-file");
+        BufferedFile bufferedFile = BufferedFile.create(tempFile.toString()).get(1000, TimeUnit.MILLISECONDS);
+        assertEquals(0, Files.size(tempFile));
         String str = prepareString(100);
         byte[] bytes = str.getBytes(StandardCharsets.UTF_8);
         ByteBuffer byteBuffer = ByteBuffer.allocateDirect(bytes.length);
         byteBuffer.put(bytes);
         Integer wrote = bufferedFile.write(0, bytes.length, byteBuffer).get(1000, TimeUnit.MILLISECONDS);
-        assertEquals((int) tempFile.length(), wrote);
-        assertEquals(str, readFileToString(tempFile));
+        assertEquals((int) Files.size(tempFile), wrote);
+        assertEquals(str, new String(Files.readAllBytes(tempFile)));
     }
 
     @Test
@@ -183,10 +160,9 @@ public class BufferedFileTest {
     }
 
     @Test
-    void close() throws Exception {
-        File tempFile = File.createTempFile("temp-", "-file");
-        tempFile.deleteOnExit();
-        BufferedFile bufferedFile = BufferedFile.open(tempFile.getPath()).get(1000, TimeUnit.MILLISECONDS);
+    void close(@TempDir Path tmpDir) throws Exception {
+        Path tempFile = Files.createTempFile(tmpDir, "temp-", "-file");
+        BufferedFile bufferedFile = BufferedFile.open(tempFile.toString()).get(1000, TimeUnit.MILLISECONDS);
         assertTrue(bufferedFile.getRawFd() > 0);
         bufferedFile.close().get(1000, TimeUnit.MILLISECONDS);
         CompletableFuture<Integer> read = bufferedFile.read(0, ByteBuffer.allocateDirect(10));
@@ -199,10 +175,7 @@ public class BufferedFileTest {
         File file = new File(bufferedFile.getPath());
         file.deleteOnExit();
         String resultString = prepareString(100);
-        FileWriter fw = new FileWriter(file);
-        fw.write(resultString);
-        fw.flush();
-        fw.close();
+        writeStringToFile(resultString, file);
         int expectedLength = resultString.getBytes(StandardCharsets.UTF_8).length;
         long actualLength = bufferedFile.size().get(1000, TimeUnit.MILLISECONDS);
         assertEquals(expectedLength, actualLength);
@@ -247,10 +220,7 @@ public class BufferedFileTest {
         file.deleteOnExit();
         String resultString = prepareString(100);
         long stringLength = resultString.getBytes(StandardCharsets.UTF_8).length;
-        FileWriter fw = new FileWriter(file);
-        fw.write(resultString);
-        fw.flush();
-        fw.close();
+        writeStringToFile(resultString, file);
         assertEquals(stringLength, bufferedFile.size().get(1000, TimeUnit.MILLISECONDS));
         assertEquals(0, bufferedFile.preAllocate(stringLength * 2).get(1000, TimeUnit.MILLISECONDS));
         assertEquals(stringLength * 2, bufferedFile.size().get(1000, TimeUnit.MILLISECONDS));
@@ -263,10 +233,7 @@ public class BufferedFileTest {
         file.deleteOnExit();
         String resultString = prepareString(100);
         long stringLength = resultString.getBytes(StandardCharsets.UTF_8).length;
-        FileWriter fw = new FileWriter(file);
-        fw.write(resultString);
-        fw.flush();
-        fw.close();
+        writeStringToFile(resultString, file);
         assertEquals(stringLength, bufferedFile.size().get(1000, TimeUnit.MILLISECONDS));
         assertEquals(0, bufferedFile.preAllocate(stringLength, stringLength).get(1000, TimeUnit.MILLISECONDS));
         assertEquals(stringLength * 2, bufferedFile.size().get(1000, TimeUnit.MILLISECONDS));
