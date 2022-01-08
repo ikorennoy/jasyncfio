a4aa128b931bdb82f230534f3c5b393ee96df8eb
ikorennoy <korennoy.ilya1@gmail.com>
2022-01-08 08:40:07 +0300

    add arm fix

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -5,5 +5,5 @@ set -ex
 cd "$(dirname "$(dirname "$0")")"/..
 
 apt-get update -y
-apt-get install -y --no-install-recommends openjdk-8-jdk make gcc g++ libc6-dev
+apt-get install -y --no-install-recommends openjdk-8-jdk-headless make gcc libc6-dev
 ./gradlew build
