423df105de907ae4a953444a2dcffcec8cfa4afb
ikorennoy <korennoy.ilya@gmail.com>
2022-02-20 22:45:03 +0300

    BufferedFileChannel.java impl and tests

--- src/main/java/one/jasyncfio/BufferedFile.java
+++ src/main/java/one/jasyncfio/BufferedFile.java
@@ -10,8 +10,8 @@ public class BufferedFile extends AbstractFile {
     }
 
     /**
-     * Reads data at the specified position into a buffer.
-     * For {@link  DmaFile} position must be properly aligned
+     * Reads data at the specified position into the buffer.
+     * For {@link  DmaFile} position and buffer must be properly aligned
      *
      * @param position start position
      * @param buffer   The buffer into which bytes are to be transferred
@@ -19,5 +19,17 @@ public class BufferedFile extends AbstractFile {
      */
     public CompletableFuture<Integer> read(long position, ByteBuffer buffer) {
         return read(position, buffer.capacity(), buffer);
+    }
+
+    /**
+     * Writes data to the specified position into the file.
+     * For {@link DmaFile} position and buffer must be properly aligned
+     *
+     * @param position start position
+     * @param buffer   The buffer from which bytes are to be transferred
+     * @return {@link  CompletableFuture} with the number of bytes written
+     */
+    public CompletableFuture<Integer> write(long position, ByteBuffer buffer) {
+        return write(position, buffer.capacity(), buffer);
     }
 }
