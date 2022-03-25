467d3335b20d8dd5c5fc568b1a6745e639a85c12
ikorennoy <korennoy.ilya@gmail.com>
2022-03-25 19:09:29 +0300

    check ulimit

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -4,6 +4,8 @@ set -ex
 
 cd "$(dirname "$(dirname "$0")")"/..
 
+echo "ULIMIT: "
+ulimit -l
 apt-get update -y
 apt-get install -y --no-install-recommends openjdk-8-jdk make gcc g++ libc6-dev
 ./gradlew build --no-daemon --debug
