47dceced95de09e40860955e41db29aeb4e37ef6
ikorennoy <korennoy.ilya@gmail.com>
2022-03-26 02:55:27 +0300

    Add compile native lib with gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,60 +15,96 @@ tasks.filterIsInstance<JavaCompile>().forEach { compileJava ->
 }
 
 
-val cWorkDir = "src/main/c"
-val generatedHeaders = "build/generated/headers"
-val jdkPath = System.getenv("JAVA_HOME") ?: "/usr/lib/jvm/java-8-openjdk-amd64"
-val sharedLib = "build/libjasyncfio.so"
-
-
 repositories {
     mavenCentral()
 }
 
-task("prepareHeaders", Exec::class) {
-    commandLine = listOf(
-        "javah",
-        "-classpath",
-        "build/classes/java/main",
-        "-jni",
-        "-v",
-        "-d",
-        generatedHeaders,
-        "one.jasyncfio.natives.FileIoConstants",
-        "one.jasyncfio.natives.Native",
-        "one.jasyncfio.natives.UringConstants"
-    )
+
+// compile shared lib
+
+val cWorkDir = File("src/main/c")
+val objectsOutputDir: File = File("build/generated/objectFiles")
+
+val generatedHeaders = File("build/generated/headers").absolutePath
+val objectFiles = File("build/generated/objectFiles")
+if (!objectFiles.exists()) {
+    objectFiles.mkdirs()
+}
+val jdkPath = File(System.getenv("JAVA_HOME") ?: "/usr/lib/jvm/java-8-openjdk-amd64")
+val fileIoConstantsSource = File("src/main/c/file_io_constants.c")
+val sharedLib = File("build/libjasyncfio.so").absolutePath
+
+
+val syscallTarget = File(objectsOutputDir, "syscall.o")
+val javaIoUringNativesTarget = File(objectsOutputDir, "java_io_uring_natives.o")
+val ioUringConstantsTarget = File(objectsOutputDir, "io_uring_constants.o")
+val fileIoConstantsTarget = File(objectsOutputDir, "file_io_constants.o")
+
+
+task("fileIoConstants", Exec::class) {
+    val fileIoConstantsSource = File("src/main/c/file_io_constants.c")
+    workingDir = cWorkDir
+    commandLine = getCompileObjectArgs(fileIoConstantsSource, fileIoConstantsTarget)
 }
 
-task("compileLib", Exec::class) {
-    dependsOn.add(tasks.getByName("prepareHeaders"))
-    workingDir = File(cWorkDir).absoluteFile
-    println("JDK: $jdkPath")
-    println("Working dir: $workingDir")
-    println("Shared lib: ${File(sharedLib).absolutePath}")
+task("ioUringConstants", Exec::class) {
+    val ioUringConstantsSrc = File("src/main/c/io_uring_constants.c")
+    workingDir = cWorkDir
+    commandLine = getCompileObjectArgs(ioUringConstantsSrc, ioUringConstantsTarget)
+}
 
+task("javaIoUringNatives", Exec::class) {
+    val javaIoUringNativesSource = File("src/main/c/java_io_uring_natives.c")
+    workingDir = cWorkDir
+    commandLine = getCompileObjectArgs(javaIoUringNativesSource, javaIoUringNativesTarget)
+}
+
+task("syscall", Exec::class) {
+    val syscallSource = File("src/main/c/syscall.c")
+    workingDir = cWorkDir
+    commandLine = getCompileObjectArgs(syscallSource, syscallTarget)
+}
+
+task("sharedLib", Exec::class) {
+    dependsOn(
+        tasks.getByName("fileIoConstants"),
+        tasks.getByName("ioUringConstants"),
+        tasks.getByName("javaIoUringNatives"),
+        tasks.getByName("syscall")
+    )
     commandLine = listOf(
         "gcc",
-        "-g",
-        "-Ofast",
         "-shared",
-        "-fpic",
         "-o",
-        File(sharedLib).absolutePath,
-        "-I",
-        generatedHeaders,
-        "-I",
-        "$jdkPath/include",
-        "-I",
-        "$jdkPath/include/linux/",
-        "-I",
-        cWorkDir,
-        "java_io_uring_natives.c"
+        sharedLib,
+        syscallTarget.absolutePath,
+        javaIoUringNativesTarget.absolutePath,
+        ioUringConstantsTarget.absolutePath,
+        fileIoConstantsTarget.absolutePath
     )
 }
 
+fun getCompileObjectArgs(sourceFile: File, outputFile: File): List<String> {
+    return listOf(
+        "gcc",
+        "-c",
+        "-g",
+        "-Ofast",
+        "-D_GNU_SOURCE",
+        "-fpic",
+        "-o",
+        outputFile.absolutePath,
+        "-I",
+        "${jdkPath.absolutePath}/include",
+        "-I",
+        "${jdkPath.absolutePath}/include/linux",
+        sourceFile.absolutePath
+    )
+}
+
+
 tasks.jar {
-    dependsOn.add(tasks.getByName("compileLib"))
+    dependsOn.add(tasks.getByName("sharedLib"))
     from(sharedLib)
     archiveClassifier.set("linux-x86_64")
     manifest {
