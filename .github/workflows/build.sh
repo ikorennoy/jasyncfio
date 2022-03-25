62e8fd9fd2af55bedd117439a8db5510eca7a55a
ikorennoy <korennoy.ilya@gmail.com>
2022-03-25 19:11:29 +0300

    check ulimit

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -4,7 +4,7 @@ set -ex
 
 cd "$(dirname "$(dirname "$0")")"/..
 
-echo "ULIMIT: "
+echo "ULIMIT1233: "
 ulimit -l
 apt-get update -y
 apt-get install -y --no-install-recommends openjdk-8-jdk make gcc g++ libc6-dev
