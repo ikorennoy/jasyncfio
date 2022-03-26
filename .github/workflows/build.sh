f7a34a691aa85aa27d2d7a8ffef666dff1f752f1
Ilya Korennoy <30526160+ikorennoy@users.noreply.github.com>
2022-03-26 12:21:21 +0300

    Publication and fix for CI build (#17)
    * Add some publishing stuff
    
    * check ulimit
    
    * check ulimit
    
    * change ulimit
    
    * Delete debug
    
    * Delete debug
    
    * Add source and target versions
    
    * Try arm
    
    * Try release
    Remove arm
    
    * add artifact publish
    
    * re-run
    
    * add publication
    
    * fix
    
    * fix
    
    * ad ls
    
    * ad ls
    
    * ad ls
    
    * return checkout
    
    * return checkout
    
    * try uraimo/run-on-arch-action@v2
    
    * incr timeout
    
    * change arch
    
    * try aarch64 again
    
    * get rid of nokie
    
    * add header generation
    
    * Add compile native lib with gradle
    
    * Add compile native lib with gradle
    
    * add java home print
    
    * Detect arch
    
    * check where java
    
    * Some fixes with path
    
    * print java home
    
    * change install deps location
    
    * Return deps to install
    
    * Add ulimit
    
    * Rollback to prev builds
    
    * Rollback to prev builds
    
    * add additional docker commands
    
    * Add debug to build
    
    * delete arm64 build

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -7,3 +7,4 @@ cd "$(dirname "$(dirname "$0")")"/..
 apt-get update -y
 apt-get install -y --no-install-recommends openjdk-8-jdk make gcc g++ libc6-dev
 ./gradlew build --no-daemon --debug
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,132 @@
 plugins {
     java
-    id("dev.nokee.jni-library")
-    id("dev.nokee.c-language")
+    `maven-publish`
     id("me.champeau.jmh") version "0.6.1"
 }
 
+val jvmTargetVersion = "8"
+
 group = "one.jasyncfio"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
+
+tasks.filterIsInstance<JavaCompile>().forEach { compileJava ->
+    compileJava.targetCompatibility = jvmTargetVersion
+    compileJava.sourceCompatibility = jvmTargetVersion
+}
+
 
 repositories {
     mavenCentral()
 }
 
+
+// compile shared lib
+
+val arch: String
+    get() {
+        val archString = System.getProperty("os.arch")
+        return if ("x86_64".equals(archString, true) || "amd64".equals(archString, true)) {
+            "amd64"
+        } else if ("aarch64".equals(archString, true)) {
+            "arm64"
+        } else {
+            throw IllegalArgumentException("Architecture $archString is not supported")
+        }
+    }
+
+val jdkPath: File
+    get() {
+        val javaHome = System.getenv("JAVA_HOME") ?: "/usr/lib/jvm/java-8-openjdk-$arch"
+        return File(javaHome)
+    }
+
+println("JAVA_HOME: $jdkPath")
+println("ARCH: $arch")
+
+val cWorkDir = File("src/main/c")
+val objectsOutputDir: File = File("build/generated")
+val sharedLib = File("build/libjasyncfio.so").absolutePath
+
+// all targets
+val syscallTarget = File(objectsOutputDir, "syscall.o")
+val javaIoUringNativesTarget = File(objectsOutputDir, "java_io_uring_natives.o")
+val ioUringConstantsTarget = File(objectsOutputDir, "io_uring_constants.o")
+val fileIoConstantsTarget = File(objectsOutputDir, "file_io_constants.o")
+
+
+task("fileIoConstants", Exec::class) {
+    val fileIoConstantsSource = File("src/main/c/file_io_constants.c")
+    workingDir = cWorkDir
+    commandLine = getCompileObjectArgs(fileIoConstantsSource, fileIoConstantsTarget)
+}
+
+task("ioUringConstants", Exec::class) {
+    val ioUringConstantsSrc = File("src/main/c/io_uring_constants.c")
+    workingDir = cWorkDir
+    commandLine = getCompileObjectArgs(ioUringConstantsSrc, ioUringConstantsTarget)
+}
+
+task("javaIoUringNatives", Exec::class) {
+    val javaIoUringNativesSource = File("src/main/c/java_io_uring_natives.c")
+    workingDir = cWorkDir
+    commandLine = getCompileObjectArgs(javaIoUringNativesSource, javaIoUringNativesTarget)
+}
+
+task("syscall", Exec::class) {
+    val syscallSource = File("src/main/c/syscall.c")
+    workingDir = cWorkDir
+    commandLine = getCompileObjectArgs(syscallSource, syscallTarget)
+}
+
+task("sharedLib", Exec::class) {
+    dependsOn(
+        tasks.getByName("fileIoConstants"),
+        tasks.getByName("ioUringConstants"),
+        tasks.getByName("javaIoUringNatives"),
+        tasks.getByName("syscall")
+    )
+    commandLine = listOf(
+        "gcc",
+        "-shared",
+        "-o",
+        sharedLib,
+        syscallTarget.absolutePath,
+        javaIoUringNativesTarget.absolutePath,
+        ioUringConstantsTarget.absolutePath,
+        fileIoConstantsTarget.absolutePath
+    )
+}
+
+fun getCompileObjectArgs(sourceFile: File, outputFile: File): List<String> {
+    return listOf(
+        "gcc",
+        "-c",
+        "-g",
+        "-Ofast",
+        "-D_GNU_SOURCE",
+        "-fpic",
+        "-o",
+        outputFile.absolutePath,
+        "-I",
+        "${jdkPath.absolutePath}/include",
+        "-I",
+        "${jdkPath.absolutePath}/include/linux",
+        sourceFile.absolutePath
+    )
+}
+
+
 tasks.jar {
+    dependsOn.add(tasks.getByName("sharedLib"))
+    from(sharedLib)
+    archiveClassifier.set("linux-$arch")
     manifest {
-        attributes(mapOf("Implementation-Title" to project.name,
-            "Implementation-Version" to project.version,
-            "Bundle-Description" to "jasyncfio provides an API for working with files through the Linux io_uring interface"),
+        attributes(
+            mapOf(
+                "Implementation-Title" to project.name,
+                "Implementation-Version" to project.version,
+                "Bundle-Description" to "jasyncfio provides an API for working with files through the Linux io_uring interface"
+            ),
         )
     }
 }
@@ -25,9 +135,38 @@ tasks.withType(Test::class) {
     useJUnitPlatform()
 }
 
-tasks.withType(CCompile::class.java) {
-    compilerArgs.add("-ggdb")
-    compilerArgs.add("-D_GNU_SOURCE")
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            pom {
+                name.set("jasyncfio")
+                description.set("jasyncfio provides an API for working with files through the Linux io_uring interface")
+                url.set("https://github.com/ikorennoy/jasyncfio")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("ikorennoy")
+                        name.set("Ilya Korennoy")
+                        email.set("korennoy.ilya@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/ikorennoy/jasyncfio")
+                    developerConnection.set("scm:git:ssh://git@github.com/ikorennoy/jasyncfio.git")
+                    url.set("https://github.com/ikorennoy/jasyncfio")
+                }
+            }
+            groupId = "one.jasyncfio"
+            artifactId = "jasyncfio"
+            version = version
+            from(components["java"])
+        }
+    }
 }
 
 dependencies {
