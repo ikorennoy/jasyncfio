0e385d2ed630c8d2978aa79a3a9cfd6a43639818
Ilya Korennoy <30526160+ikorennoy@users.noreply.github.com>
2022-04-06 18:03:32 +0300

    New create open methods (#26)
    * Support OpenOption for BufferedFile
    
    * Support OpenOption for DmaFile

--- src/main/c/file_io_constants.c
+++ src/main/c/file_io_constants.c
@@ -30,16 +30,36 @@ static jint get_o_direct() {
 static jint get_cloexec() {
     return O_CLOEXEC;
 }
+static jint get_append() {
+    return O_APPEND;
+}
+static jint get_dsync() {
+    return O_DSYNC;
+}
+static jint get_excl() {
+    return O_EXCL;
+}
+static jint get_noatime() {
+    return O_NOATIME;
+}
+static jint get_sync() {
+    return O_SYNC;
+}
 
 static JNINativeMethod method_table[] = {
-    {"oRdOnly", "()I", (void *) get_o_rdonly},
-    {"oWrOnly", "()I", (void *) get_o_wronly},
-    {"oRdWr", "()I", (void *) get_o_rdwr},
-    {"oCreat", "()I", (void *) get_o_creat},
-    {"oTrunc", "()I", (void *) get_o_trunc},
+    {"oRdOnly",   "()I", (void *) get_o_rdonly},
+    {"oWrOnly",   "()I", (void *) get_o_wronly},
+    {"oRdWr",     "()I", (void *) get_o_rdwr},
+    {"oCreat",    "()I", (void *) get_o_creat},
+    {"oTrunc",    "()I", (void *) get_o_trunc},
     {"statxSize", "()I", (void *) get_statx_size},
-    {"oDirect", "()I", (void *) get_o_direct},
-    {"oCloexec", "()I", (void *) get_cloexec},
+    {"oDirect",   "()I", (void *) get_o_direct},
+    {"oCloexec",  "()I", (void *) get_cloexec},
+    {"oAppend",   "()I", (void *) get_append},
+    {"oDsync",    "()I", (void *) get_dsync},
+    {"oExcl",     "()I", (void *) get_excl},
+    {"oNoAtime",  "()I", (void *) get_noatime},
+    {"oSync",     "()I", (void *) get_sync},
 };
 
 jint jni_file_io_constants_on_load(JNIEnv *env) {
