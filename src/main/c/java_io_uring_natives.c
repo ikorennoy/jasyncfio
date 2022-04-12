79d34008347b20e0b5e0445311ec1e2db6fbc1b1
ikorennoy <korennoy.ilya@gmail.com>
2022-04-12 20:16:42 +0300

    Refactor C code

--- src/main/c/java_io_uring_natives.c
+++ src/main/c/java_io_uring_natives.c
@@ -41,50 +41,72 @@
     }
 #endif
 
+static void io_uring_unmap_rings(struct io_uring_sq *sq, struct io_uring_cq *cq) {
+     munmap(sq->ring_ptr, sq->ring_sz);
+     if (cq->ring_ptr && cq->ring_ptr != sq->ring_ptr)
+         munmap(cq->ring_ptr, cq->ring_sz);
+}
 
-int io_uring_mmap(struct io_uring *ring, struct io_uring_params *p, struct io_uring_sq *sq, struct io_uring_cq *cq) {
+int io_uring_mmap(int fd, struct io_uring_params *p,
+        struct io_uring_sq *sq, struct io_uring_cq *cq) {
+    int ret;
     size_t size;
 
     sq->ring_sz = p->sq_off.array + p->sq_entries * sizeof(unsigned);
     cq->ring_sz = p->cq_off.cqes + p->cq_entries * sizeof(struct io_uring_cqe);
 
-    sq->ring_ptr = mmap(0, sq->ring_sz, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_POPULATE, ring->ring_fd,
+    if (p->features & IORING_FEAT_SINGLE_MMAP) {
+        if (cq->ring_sz > sq->ring_sz)
+            sq->ring_sz = cq->ring_sz;
+        cq->ring_sz = sq->ring_sz;
+    }
+
+    sq->ring_ptr = mmap(0, sq->ring_sz, PROT_READ | PROT_WRITE,
+                        MAP_SHARED | MAP_POPULATE, fd,
                         IORING_OFF_SQ_RING);
 
     if (sq->ring_ptr == MAP_FAILED) {
-        return -errno;
+        return errno;
     }
 
-    cq->ring_ptr = mmap(0, cq->ring_sz, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_POPULATE, ring->ring_fd,
-                        IORING_OFF_CQ_RING);
-
-    if (cq->ring_ptr == MAP_FAILED) {
-        return -errno;
+    if (p->features & IORING_FEAT_SINGLE_MMAP) {
+        cq->ring_ptr = sq->ring_ptr;
+    } else {
+        cq->ring_ptr = mmap(0, cq->ring_sz, PROT_READ | PROT_WRITE,
+                             MAP_SHARED | MAP_POPULATE, fd, IORING_OFF_CQ_RING);
+        if (cq->ring_ptr == MAP_FAILED) {
+            ret = errno;
+            cq->ring_ptr = NULL;
+            goto err;
+        }
     }
 
-    sq->head = sq->ring_ptr + p->sq_off.head;
-    sq->tail = sq->ring_ptr + p->sq_off.tail;
-    sq->ring_mask = sq->ring_ptr + p->sq_off.ring_mask;
-    sq->ring_entries = sq->ring_ptr + p->sq_off.ring_entries;
-    sq->flags = sq->ring_ptr + p->sq_off.flags;
-    sq->dropped = sq->ring_ptr + p->sq_off.dropped;
+    sq->khead = sq->ring_ptr + p->sq_off.head;
+    sq->ktail = sq->ring_ptr + p->sq_off.tail;
+    sq->kring_mask = sq->ring_ptr + p->sq_off.ring_mask;
+    sq->kring_entries = sq->ring_ptr + p->sq_off.ring_entries;
+    sq->kflags = sq->ring_ptr + p->sq_off.flags;
+    sq->kdropped = sq->ring_ptr + p->sq_off.dropped;
     sq->array = sq->ring_ptr + p->sq_off.array;
 
     size = p->sq_entries * sizeof(struct io_uring_sqe);
 
-    sq->sqes = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_POPULATE, ring->ring_fd, IORING_OFF_SQES);
+    sq->sqes = mmap(0, size, PROT_READ | PROT_WRITE,
+                    MAP_SHARED | MAP_POPULATE, fd, IORING_OFF_SQES);
+
     if (sq->sqes == MAP_FAILED) {
-        return -errno;
+        ret = errno;
+err:
+        io_uring_unmap_rings(sq, cq);
+        return ret;
     }
 
-    cq->head = cq->ring_ptr + p->cq_off.head;
-    cq->tail = cq->ring_ptr + p->cq_off.tail;
-    cq->ring_mask = cq->ring_ptr + p->cq_off.ring_mask;
-    cq->ring_entries = cq->ring_ptr + p->cq_off.ring_entries;
-    cq->overflow = cq->ring_ptr + p->cq_off.overflow;
+    cq->khead = cq->ring_ptr + p->cq_off.head;
+    cq->ktail = cq->ring_ptr + p->cq_off.tail;
+    cq->kring_mask = cq->ring_ptr + p->cq_off.ring_mask;
+    cq->kring_entries = cq->ring_ptr + p->cq_off.ring_entries;
+    cq->koverflow = cq->ring_ptr + p->cq_off.overflow;
     cq->cqes = cq->ring_ptr + p->cq_off.cqes;
-
-    ring->flags = p->flags;
 
     return 0;
 }
@@ -156,9 +178,10 @@ void throwRuntimeExceptionErrorNo(JNIEnv* env, char* message, int errorNumber) {
     free(allocatedMessage);
 }
 
-int setup_iouring(JNIEnv *env, struct io_uring *ring, int entries, int flags, int sq_thread_idle, int sq_thread_cpu, int cq_size, int attach_wq_ring_fd) {
+void setup_iouring(JNIEnv *env, struct io_uring *ring, int entries, int flags, int sq_thread_idle, int sq_thread_cpu, int cq_size, int attach_wq_ring_fd) {
     struct io_uring_params p;
     int ring_fd;
+    int ret;
 
     memset(&p, 0, sizeof(p));
     if (flags & IORING_SETUP_SQPOLL) {
@@ -177,18 +200,23 @@ int setup_iouring(JNIEnv *env, struct io_uring *ring, int entries, int flags, in
     ring_fd = sys_io_uring_setup(entries, &p);
     if (ring_fd < 0) {
         throwRuntimeExceptionErrorNo(env, "failed to create io_uring ring fd;", errno);
-        return NULL;
     }
 
-    ring->ring_fd = ring_fd;
+    ret = io_uring_mmap(ring_fd, &p, &ring->sq, &ring->cq);
 
-    return io_uring_mmap(ring, &p, &ring->sq, &ring->cq);
+    if (!ret) {
+        ring->flags = p.flags;
+        ring->ring_fd = ring_fd;
+    } else {
+        close(ring_fd);
+        throwRuntimeExceptionErrorNo(env, "failed to create io_uring ring fd;", errno);
+    }
 }
 
 static void java_io_uring_register(JNIEnv *env, jclass clazz, jint fd, jint opcode, jlong arg, jint nr_args) {
     int result;
 
-    result = sys_io_uring_register(fd, opcode, arg, nr_args);
+    result = sys_io_uring_register(fd, opcode, (void *) arg, nr_args);
     if (result < 0) {
         throwRuntimeExceptionErrorNo(env, "failed to call sys_io_uring_register", errno);
     }
@@ -214,15 +242,15 @@ static jobjectArray java_io_uring_setup_iouring(JNIEnv *env, jclass clazz, jint 
     }
 
     struct io_uring ring;
-    int res = setup_iouring(env, &ring, entries, flags, sq_thread_idle, sq_thread_cpu, cq_size, attach_wq_ring_fd);
+    setup_iouring(env, &ring, entries, flags, sq_thread_idle, sq_thread_cpu, cq_size, attach_wq_ring_fd);
 
     jlong submissionArrayElements[] = {
-        (jlong) ring.sq.head,
-        (jlong) ring.sq.tail,
-        (jlong) ring.sq.ring_mask,
-        (jlong) ring.sq.ring_entries,
-        (jlong) ring.sq.flags,
-        (jlong) ring.sq.dropped,
+        (jlong) ring.sq.khead,
+        (jlong) ring.sq.ktail,
+        (jlong) ring.sq.kring_mask,
+        (jlong) ring.sq.kring_entries,
+        (jlong) ring.sq.kflags,
+        (jlong) ring.sq.kdropped,
         (jlong) ring.sq.array,
         (jlong) ring.sq.sqes,
         (jlong) ring.sq.ring_sz,
@@ -233,11 +261,11 @@ static jobjectArray java_io_uring_setup_iouring(JNIEnv *env, jclass clazz, jint 
     (*env)->SetLongArrayRegion(env, submissionArray, 0, 12, submissionArrayElements);
 
     jlong completionArrayElements[] = {
-        (jlong) ring.cq.head,
-        (jlong) ring.cq.tail,
-        (jlong) ring.cq.ring_mask,
-        (jlong) ring.cq.ring_entries,
-        (jlong) ring.cq.overflow,
+        (jlong) ring.cq.khead,
+        (jlong) ring.cq.ktail,
+        (jlong) ring.cq.kring_mask,
+        (jlong) ring.cq.kring_entries,
+        (jlong) ring.cq.koverflow,
         (jlong) ring.cq.cqes,
         (jlong) ring.cq.ring_ptr,
         (jlong) ring.cq.ring_sz,
