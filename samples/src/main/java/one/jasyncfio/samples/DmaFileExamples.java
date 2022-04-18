e5bd08baca33cce84f2742e5174e3b65badc16d9
ikorennoy <korennoy.ilya@gmail.com>
2022-04-18 23:43:54 +0300

    Add DmaFile example

--- samples/src/main/java/one/jasyncfio/samples/DmaFileExamples.java
+++ samples/src/main/java/one/jasyncfio/samples/DmaFileExamples.java
@@ -1,6 +1,12 @@
 package one.jasyncfio.samples;
 
+import one.jasyncfio.DmaFile;
 import one.jasyncfio.EventExecutorGroup;
+import one.jasyncfio.MemoryUtils;
+import one.jasyncfio.OpenOption;
+
+import java.nio.ByteBuffer;
+import java.util.UUID;
 
 public class DmaFileExamples {
 
@@ -9,11 +15,20 @@ public class DmaFileExamples {
      * In this example we will use all the power of io_uring and make a read without a single syscall,
      * plus we will use busy-wait instead of IRQ to wait for the operation to be ready
      */
-    public void readSqPollIoPoll() throws Exception {
-        EventExecutorGroup
-                .builder()
-                .numberOfRings(1)
-                .build();
+    public void write() throws Exception {
+        EventExecutorGroup eventExecutorGroup = EventExecutorGroup.initDefault();
+        DmaFile dmaFile = eventExecutorGroup.openDmaFile(prepareFilePath(), OpenOption.READ_ONLY, OpenOption.CREATE).get();
+
+        // allocate aligned buffer
+        ByteBuffer alignedByteBuffer = MemoryUtils.allocateAlignedByteBuffer(512, DmaFile.DEFAULT_ALIGNMENT);
+
+        Integer written = dmaFile.write(0, 512, alignedByteBuffer).get();
 
     }
+
+
+    private String prepareFilePath() {
+        return "/tmp/" + UUID.randomUUID();
+    }
+
 }
