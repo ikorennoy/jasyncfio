9f5982de35e5890ea54cb5699750fd74eefbc0e8
Ilya Korennoy <korennoy.ilya@gmail.com>
2022-05-14 00:16:06 +0300

    Fix release

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -8,7 +8,7 @@ apt-get update -y
 apt-get install -y --no-install-recommends openjdk-8-jdk make gcc g++ libc6-dev
 
 release() {
-  ./gradlew clean build publishToSonatype closeAndReleaseSonatypeStagingRepository -Pversion=$VERSION -Psigning.keyId=D7B979C7 -Psigning.password=$PASSPHRASE -Psigning.secretKeyRingFile=$GITHUB_WORKSPACE/release.gpg
+  ./gradlew clean build publishToSonatype closeAndReleaseSonatypeStagingRepository -Pversion=$VERSION -Psigning.gnupg.passphrase=$PASSPHRASE
 }
 
 build() {
