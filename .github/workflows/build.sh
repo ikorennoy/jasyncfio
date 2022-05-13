2afe3dfb64455a4e2d96b9cd30e7671132875388
Ilya Korennoy <30526160+ikorennoy@users.noreply.github.com>
2022-05-13 22:58:09 +0300

    Fix build.sh (#48)
    * Fix build.sh
    
    * Fix

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -7,6 +7,14 @@ cd "$(dirname "$(dirname "$0")")"/..
 apt-get update -y
 apt-get install -y --no-install-recommends openjdk-8-jdk make gcc g++ libc6-dev
 
+release() {
+  ./gradlew clean build publishToSonatype -Pversion=$VERSION -Psigning.gnupg.passphrase=$PASSPHRASE
+}
+
+build() {
+  ./gradlew build --no-daemon
+}
+
 case "$1" in
   "release")
     release
@@ -18,11 +26,3 @@ case "$1" in
     echo "You have to specify release or build."
     exit 1
 esac
-
-function release() {
-  ./gradlew clean build publishToSonatype -Pversion=$VERSION -Psigning.gnupg.passphrase=$PASSPHRASE
-}
-
-function build() {
-  ./gradlew build --no-daemon
-}
