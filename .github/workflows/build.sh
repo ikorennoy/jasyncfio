dffb9274214c983118e2ae28aecfeb3c68a239b7
ikorennoy <korennoy.ilya@gmail.com>
2022-05-14 00:27:22 +0300

    Install gnupg

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -5,7 +5,7 @@ set -ex
 cd "$(dirname "$(dirname "$0")")"/..
 
 apt-get update -y
-apt-get install -y --no-install-recommends openjdk-8-jdk make gcc g++ libc6-dev
+apt-get install -y --no-install-recommends openjdk-8-jdk make gcc g++ libc6-dev gnupg
 
 release() {
   ./gradlew clean build publishToSonatype -Pversion=$VERSION -Psigning.gnupg.passphrase=$PASSPHRASE
