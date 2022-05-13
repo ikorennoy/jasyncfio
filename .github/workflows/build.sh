4135eb940761ed4ef5109c1aed1dc08cbcce24a2
ikorennoy <korennoy.ilya@gmail.com>
2022-05-13 21:15:57 +0300

    Add jdk8

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -4,6 +4,13 @@ set -ex
 
 cd "$(dirname "$(dirname "$0")")"/..
 
+# Add stretch for Java 8
+cat <<END > /etc/apt/sources.list.d/stretch.list
+deb http://deb.debian.org/debian stretch main
+deb http://security.debian.org/debian-security stretch/updates main
+END
+
+
 apt-get update -y
 apt-get install -y --no-install-recommends openjdk-8-jdk make gcc g++ libc6-dev
 ./gradlew build --no-daemon
